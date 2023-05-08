6913c98e33c0c66b9a10045b469a6936431b5f59
nero <wnsgur765z@naver.com>
2023-05-08 23:12:25 +0900

    Refactor: tweet controller

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
-import { config } from './config.js';
-import { initSocket } from './connection/socket.js';
-import { sequelize } from './db/database.js';
+import {config} from './config.js';
+import {initSocket, getSocketIO} from './connection/socket.js';
+import {sequelize} from './db/database.js';
+import {TweetController} from './controller/tweet.js';
+import * as tweetRepository from './data/tweet';
 
 const app = express();
 
@@ -20,7 +22,9 @@ app.use(helmet());
 app.use(cors(corsOption));
 app.use(morgan('tiny'));
 
-app.use('/tweets', tweetsRouter);
+app.use('/tweets',
+  tweetsRouter(new TweetController(tweetRepository, getSocketIO))
+);
 app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
--- middleware/tests/validator.test.js
+++ middleware/tests/validator.test.js
@@ -7,19 +7,30 @@ jest.mock('express-validator');
 
 describe('Validator Middleware', () => {
   it('calls next if there are no validation errors', () => {
-    const request = httpMocks.createRequest({
-      method: 'GET',
-      url: '/tweets',
-    });
+    const request = httpMocks.createRequest();
     const response = httpMocks.createResponse();
     const next = jest.fn();
-    validator.validationResult = jest.fn(() => ({isEmpty: () => true}))
-
+    validator.validationResult = jest.fn(() => (
+      {isEmpty: () => true}))
 
     validate(request, response, next);
+    expect(next).toBeCalled();
   });
 
   it('returns 400 if there are validation errors', () => {
+    const request = httpMocks.createRequest();
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    const errorMsg = faker.random.words(3);
+    validator.validationResult = jest.fn(() => (
+      {
+        isEmpty: () => false,
+        array: () => [{msg: errorMsg}],
+      }));
 
+    validate(request, response, next);
+    expect(next).not.toBeCalled();
+    expect(response._getStatusCode()).toBe(400);
+    expect(response._getJSONData().message).toBe(errorMsg);
   });
 });
